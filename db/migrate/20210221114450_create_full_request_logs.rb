4fbe03582d51d92cf643d5ed2053f221478e1d16
Vishal Zambre <v.zambre@gmail.com>
2021-02-21 19:00:14 +0530

    Timestamp added

--- db/migrate/20210221114450_create_full_request_logs.rb
+++ db/migrate/20210221114450_create_full_request_logs.rb
@@ -3,6 +3,7 @@ class CreateFullRequestLogs < ActiveRecord::Migration
     create_table :full_request_logs do |t|
       t.string :request_id
       t.text :body
+      t.timestamps
     end
   end
 end
