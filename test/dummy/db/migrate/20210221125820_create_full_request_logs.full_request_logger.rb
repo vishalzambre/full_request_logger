fd6eadcfb9db588c7dd092e7c1e32276e4f60cef
Vishal Zambre <v.zambre@gmail.com>
2021-02-21 18:36:35 +0530

    Migration added

--- test/dummy/db/migrate/20210221125820_create_full_request_logs.full_request_logger.rb
+++ test/dummy/db/migrate/20210221125820_create_full_request_logs.full_request_logger.rb
@@ -1,5 +1,5 @@
 # This migration comes from full_request_logger (originally 20210221114450)
-class CreateFullRequestLogs < ActiveRecord::Migration
+class CreateFullRequestLogs < ActiveRecord::Migration[5.2]
   def change
     create_table :full_request_logs do |t|
       t.string :request_id
